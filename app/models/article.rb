8dc2ae0a2c2b3ef5e48ae98924dd75b52dca84f5
Noahkimtai <noahkimtai24@gmail.com>
2022-11-01 19:27:03 +0300

    Initial commit with attributes for the models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
 class Article
+    attr_accessor :title, :author_name, :date
 
+    @@all = []
+
+    def initialize
+        @title = title
+        @author_name = author_name
+        @date = date
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
