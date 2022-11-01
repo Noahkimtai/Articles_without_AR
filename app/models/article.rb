92cf89d1d12e915dc40f61f1da78e82215e33a22
Noahkimtai <noahkimtai24@gmail.com>
2022-11-01 19:36:12 +0300

    method to access the magazine name added to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,23 @@
 class Article
-    attr_accessor :title, :author_name, :date
+    attr_accessor :title, :date
 
     @@all = []
 
-    def initialize
+    def initialize(title,author,date,magazine)
         @title = title
-        @author_name = author_name
+        @author = author
         @date = date
+        @magazine =  magazine
         @@all << self
+    end
+
+    # method to access the author of a the article
+    def author
+        @author.name
+    end
+    # method for accessing the name of the magazine the article belong to
+    def magazine
+        @magazine.name
     end
 
     def self.all
