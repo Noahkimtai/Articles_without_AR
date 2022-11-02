539b5df6e4523d4a94c95484d2883baaadda9a97
Noahkimtai <noahkimtai24@gmail.com>
2022-11-03 01:15:45 +0300

    completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,19 +9,15 @@ class Article
         @title = title
         @@all << self
     end
-
     # method to access the author of a the article
     def author
         @author.name
     end
-    
     # method to access the magazine for the article
     def magazine
         @magazine.name
     end
-
     def self.all
         @@all
     end
-
 end
