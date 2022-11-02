7e5c13e3a24c2c7c7a4fa072397a6e4a248b867d
Noahkimtai <noahkimtai24@gmail.com>
2022-11-02 09:01:09 +0300

    tests added to console.rb

--- tools/console.rb
+++ tools/console.rb
@@ -6,17 +6,17 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
-at1=Author.new("Author1")
-mag1=Magazine.new("Mag1","Cat1")
-mag2=Magazine.new("Mag2","Cat1")
-mag3=Magazine.new("Mag3","Cat2")
-art1=Article.new(at1,mag1,"Title1")
-art2=Article.new(at1,mag1,"Title2")
-#at1.add_article(mag1,"Title3")
-#at1.add_article(mag1,"Title6")
-#at1.add_article(mag1,"Title7")
-#at1.add_article(mag2,"Title4")
-#at1.add_article(mag3,"Title5")
+my_at1=Author.new("Author1")
+my_mag1=Magazine.new("my_mag1","Cmy_at1")
+my_mag2=Magazine.new("my_mag2","Cmy_at1")
+my_mag3=Magazine.new("m","Cat2")
+art1=Article.new(my_at1,my_mag1,"Title1")
+art2=Article.new(my_at1,my_mag1,"Title2")
+my_at1.add_article(my_mag1,"Title3")
+my_at1.add_article(my_mag1,"Title6")
+my_at1.add_article(my_mag1,"Title7")
+my_at1.add_article(my_mag2,"Title4")
+my_at1.add_article(my_mag3,"Title5")
 
 
 
